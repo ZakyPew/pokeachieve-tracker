98e56b9248426ffc851b98c752fa1e09c068ded1
Celest <celest@pokeachieve.com>
2026-03-01 01:53:55 +0000

    Bump to v1.8.8 - Include achievements folder

--- BUILD_DIRECTORY.bat
+++ BUILD_DIRECTORY.bat
@@ -1,8 +1,8 @@
 @echo off
-REM Build PokeAchieve Tracker v1.8.7 - DIRECTORY MODE
+REM Build PokeAchieve Tracker v1.8.8 - DIRECTORY MODE
 
 echo ==========================================
-echo PokeAchieve Tracker v1.8.7 - Directory Build
+echo PokeAchieve Tracker v1.8.8 - Directory Build
 echo ==========================================
 echo.
 
